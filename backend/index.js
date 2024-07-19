8f439e361f0534b2074e0482b76bf34c934c0efe
Matt Young <mattyoung@Matts-MacBook-Pro-2019.local>
2024-07-18 22:32:43 -0600

    fixes

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,6 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from 'dotenv';
 import userRouter from "./src/api/user.js";
-import ledgerRouter from "./src/api/ledger.js"
-import wagerRouter from "./src/api/wager.js"
-import giphyRouter from "./src/api/giphy-controller.js"
 
 dotenv.config();
 
@@ -14,15 +11,10 @@ const app = express();
 app.use(bodyParser.json({ limit: "5mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 
-app.use(cors());
+// app.use(cors());
 app.use("/api/user", userRouter);
-app.use("/api/ledger", ledgerRouter);
-app.use("/api/wager", wagerRouter);
-app.use("/api/giphy", giphyRouter);
 
 const PORT = process.env.PORT || 80;
-
-console.log(process.env)
 
 mongoose
   .connect(process.env.MONGODB_URL)
